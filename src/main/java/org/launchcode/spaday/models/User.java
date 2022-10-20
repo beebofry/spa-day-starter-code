ebb9516ba5bd1b1b8e721e78c9371bb9c8ecaab8
kimlee <kiim.n.lee@gmail.com>
2022-10-19 20:32:09 -0500

    user validation studio completed

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,31 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.*;
+
 public class User {
+    @NotBlank
+    @Size(min = 5, max = 15)
     private String username;
+    @Email
     private String email;
+    @NotBlank
+    @Size(min = 6)
     private String password;
+
+    @NotNull(message = "Passwords do not match.")
+    @NotBlank
+    private String verify;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verify) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verify = verify;
     }
 
     public String getUsername() {
@@ -38,5 +50,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword(password, this.verify);
+    }
+
+    public String getVerify() {
+        return verify;
+    }
+
+    public void setVerify(String verify) {
+        this.verify = verify;
+        checkPassword(this.password, verify);
+    }
+
+    private void checkPassword(String password, String verify){
+        if(password != null && verify != null && !password.equals(verify)){
+            this.verify = null;
+        }
     }
 }
